2197c192cb8646cd11f1dacf04661a22c1deb870
Francesco De Lisi <fr.delisi@gmail.com>
2013-08-24 19:35:59 +0200

    .NET development

---
+++ ExtendedTextBlock/ExtendedButton.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMAControls
+{
+    public partial class ExtendedButton : UserControl
+    {
+        public ExtendedButton()
+        {
+            InitializeComponent();
+            //DataContext = this;
+        }
+
+        #region TextSource
+        public static readonly DependencyProperty TextSourceProperty = DependencyProperty.Register(
+            "TextSource",
+            typeof(string),
+            typeof(ExtendedButton),
+            new FrameworkPropertyMetadata(String.Empty,OnTextSourceChanged));
+
+
+        private static void OnTextSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var myBinding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                RelativeSource = RelativeSource.Self,
+                Path = new PropertyPath(e.Property),
+            };
+
+            BindingOperations.SetBinding(d, ExtendedTextBlock.TranslationKeyProperty, myBinding);
+        }
+
+        public string TextSource
+        {
+            get
+            {
+                return (string)GetValue(TextSourceProperty);
+            }
+            set
+            {
+                SetValue(TextSourceProperty, value);
+            }
+        }
+        #endregion
+
+        #region IconSource
+        public static readonly DependencyProperty IconSourceProperty = DependencyProperty.Register(
+            "IconSource",
+            typeof(ImageSource),
+            typeof(ExtendedButton),
+            new FrameworkPropertyMetadata(default(ImageSource), OnIconSourceChanged));
+
+
+        private static void OnIconSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var myBinding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                RelativeSource = RelativeSource.Self,
+                Path = new PropertyPath(e.Property),
+            };
+
+            BindingOperations.SetBinding(d, Image.SourceProperty, myBinding);
+        }
+
+        public ImageSource IconSource
+        {
+            get
+            {
+                return (ImageSource)GetValue(IconSourceProperty);
+            }
+            set
+            {
+                SetValue(IconSourceProperty, value);
+            }
+        }
+        #endregion
+
+        #region Command
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+          "Command",
+          typeof(ICommand),
+          typeof(ExtendedButton),
+          new UIPropertyMetadata(null));
+
+
+        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var myBinding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                RelativeSource = RelativeSource.Self,
+                Path = new PropertyPath(e.Property),
+            };
+
+            BindingOperations.SetBinding(d, Button.CommandProperty, myBinding);
+        }
+
+        public ICommand Command
+        {
+            get
+            {
+                return (ICommand)GetValue(CommandProperty);
+            }
+            set
+            {
+                SetValue(CommandProperty, value);
+            }
+        }
+        #endregion
+    }
+}
+
---
+++ ExtendedTextBlock/ExtendedTextBlock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using Translator;
+
+namespace IMAControls
+{
+    public class ExtendedTextBlock : TextBlock
+    {
+        /// <summary>
+        ///   Identifies the PlaceholderText dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TranslationKeyProperty = DependencyProperty.Register(
+            "TranslationKey",
+            typeof(string),
+            typeof(ExtendedTextBlock),
+            new FrameworkPropertyMetadata(string.Empty, OnTranslationKeyChanged));
+
+        
+        private static void OnTranslationKeyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var myBinding = new Binding
+            {
+                Mode = BindingMode.OneWay,
+                RelativeSource = RelativeSource.Self,
+                Path = new PropertyPath(e.Property),                
+            };
+            
+            BindingOperations.SetBinding(d, ExtendedTextBlock.TextProperty, myBinding);
+            
+            string translationKeyValue = String.Empty;
+            if (e.NewValue != null)
+                translationKeyValue = Convert.ToString(e.NewValue);
+
+            if (!String.IsNullOrEmpty(translationKeyValue))
+                d.SetValue(TextProperty, translationKeyValue.Translate());
+            else 
+            {
+                Debug.WriteLine(String.Format("Empty translation key:{0}", e.NewValue));
+            }
+        }
+
+        /// <summary>
+        ///   Gets or sets the placeholder text to be shown when text box has no text and is not in focus. This is a dependency property.
+        /// </summary>
+        public string TranslationKey
+        {
+            get
+            {
+                return (string)GetValue(TranslationKeyProperty);
+            }
+            set
+            {
+                SetValue(TranslationKeyProperty, value);
+            }
+        }
+    }
+}
